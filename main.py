39dc0d5ad6d41f8ebf5a31cbd1ba6e7c5def722f
rahmanghub <indmabdulrahman@gmail.com>
2026-01-14 16:29:12 +0530

    Adding Middleware origin(frontend link)

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ Base.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],
+    allow_origins=["https://focussprint.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
